152f1873db224484b64bdaf7e4f9cf4ad1134274
Jisu Kim <vbalien@live.jp>
2015-08-12 17:25:14 +0900

    set default configs

---
+++ jkbooru/config/default_config.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+class DefaultConfig(object):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+    UPLOAD_FOLDER = '/Users/vbalien/Documents/jkbooru/jkbooru/static/images/'
