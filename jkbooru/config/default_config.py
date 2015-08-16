7ce97c209cc0d60c3f61eb3a27b1097cd728cd51
Jisu Kim <vbalien@live.jp>
2015-08-16 18:52:09 +0900

    add jkbooru settings

--- jkbooru/config/default_config.py
+++ jkbooru/config/default_config.py
@@ -2,3 +2,6 @@
 class DefaultConfig(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
     UPLOAD_FOLDER = '/Users/vbalien/Documents/jkbooru/jkbooru/static/images/'
+    # JKBooru Configs
+    JKBOORU_TITLE = 'Live Wallpaper'
+    JKBOORU_DESCRIPTION = '배경화면 공유 페이지'
